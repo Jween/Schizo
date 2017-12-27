122dffc39a62b89271e446ed0f084baa4878e207
刘俊 <liujun@meizu.com>
2017-12-27 17:09:53 +0800

    Upgrade gradle

---
+++ app/src/main/java/com/meizu/flyme/schizo/app/SchizoApplication.java
@@ -0,0 +1,17 @@
+package com.meizu.flyme.schizo.app;
+
+import android.app.Application;
+
+/**
+ * Created by Jwn on 2017/8/30.
+ *
+ * do not fucking extend me
+ */
+
+public final class SchizoApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
