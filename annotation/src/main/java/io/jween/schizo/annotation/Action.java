af76426ba27f34710b49799c89726e2136f73637
刘俊 <liujun@meizu.com>
2018-05-23 19:27:24 +0800

    add copyright, document the schizo annotations.

--- annotation/src/main/java/io/jween/schizo/annotation/Action.java
+++ annotation/src/main/java/io/jween/schizo/annotation/Action.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.jween.io/licenses/APACHE-LICENSE-2.0.md
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.jween.schizo.annotation;
 
 import java.lang.annotation.ElementType;
@@ -6,7 +20,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Created by Jwn on 2018/1/19.
+ * Indicates that a {@linkplain SchizoService} child can be bound by this action
+ * the action should also appear inside the <intent-filter> tag of the service.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
